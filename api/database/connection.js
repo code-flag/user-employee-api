417e5bde37befe83b080aa49222601eec236a5ed
code-flag <awefrancolaz@gmail.com>
2022-07-13 13:54:12 +0100

    chore fix crashes

--- api/database/connection.js
+++ api/database/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require('dotenv/config');
 
-const url = process.env.DB_CONNECTION_URL;
+// const url = process.env.DB_CONNECTION_URL;
+const url = 'mongodb+srv://Francolaz:%40nellalinK@cluster0.kloyx.mongodb.net/?retryWrites=true&w=majority';
 const DBConnection = () => {
   mongoose.connect(
     url,
